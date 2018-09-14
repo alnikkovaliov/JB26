f0af41dee0a51905bf57e563e3ba5538659082cc
Aliaksandr Kavaliou <aliaksandr_kavaliou@epam.com>
2018-09-14 18:05:28 +0300

    Moved First up into Lesson1

---
+++ Lesson1/src/by/pog/akavaliou/main/FirstApp.java
@@ -0,0 +1,9 @@
+package by.pog.akavaliou.main;
+
+public class FirstApp {
+
+	public static void main(String[] args) {		
+		System.out.println("Hello, World!!!");		
+	}
+
+}
--- jb26-les01/src/by/pog/akavaliou/main/FirstApp.java
+++
@@ -1,8 +0,0 @@
-package by.pog.akavaliou.main;
-
-public class FirstApp {
-	
-	public static void main(String args[]) {
-		System.out.println("Hello, World!!!");		
-	}
-}
