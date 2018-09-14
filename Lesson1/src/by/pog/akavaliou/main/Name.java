dcbd73953f26a54a1ec1f4ab8cc02c64df7fa149
Aliaksandr Kavaliou <aliaksandr_kavaliou@epam.com>
2018-09-14 18:20:04 +0300

    Name task implemented

--- Lesson1/src/by/pog/akavaliou/main/Name.java
+++ Lesson1/src/by/pog/akavaliou/main/Name.java
@@ -3,6 +3,8 @@ package by.pog.akavaliou.main;
 public class Name {
 	public static void main(String[] args) {
 		
-		System.out.println("ok");
+		System.out.println(" My name is Aliaksandr Kavaliou \n "
+				+ "Address: Samakhvalavichi, Kalinina 5-15 \n "
+				+ "Phone: +375 44 718 26 86 ");
 	}
 }
